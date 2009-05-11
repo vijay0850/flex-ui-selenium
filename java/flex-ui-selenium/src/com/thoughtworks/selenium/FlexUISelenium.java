047a10bc5b23b22cae98fcf224b1af905bc3a8d7
paulocaroli <paulocaroli@7bbb1de8-3bf2-11de-9d05-31133e6853b1>
2009-05-11 13:58:34 +0000

    uploading the first version (0.1) of FlexUISeleniium, test code, and a sample flex application

---
+++ java/flex-ui-selenium/src/com/thoughtworks/selenium/FlexUISelenium.java
@@ -0,0 +1,70 @@
+package com.thoughtworks.selenium;
+
+import com.thoughtworks.selenium.FlashSelenium;
+
+public class FlexUISelenium {
+
+	private static final String TRUE = "true";
+	private static final int DEFAULT_WAIT_TIME = 5000;
+	private FlashSelenium flashApp;
+	
+	public FlexUISelenium(Selenium selenium, String flashObjectId) {
+		this.flashApp = new FlashSelenium(selenium, flashObjectId);
+	}
+
+	FlexUISelenium(FlashSelenium flashApp) {
+		this.flashApp = flashApp;
+	}
+	
+	public boolean isVisible(String objectID) {
+		return flashApp.call("getFlexVisible", objectID).equals(TRUE);
+	}
+	
+	public void waitUntilLoaded() throws Exception
+	{
+	    while (flashApp.PercentLoaded() != 100);
+	    {
+	      Thread.sleep(100);
+	    }
+    }
+	
+	public void waitUntilVisible(String objectID) throws Exception
+	{
+		this.waitUntilVisible(objectID, DEFAULT_WAIT_TIME);
+	}
+	
+	public void waitUntilVisible(String objectID, int timeout) throws Exception
+	{
+		while(timeout > 0 && ! this.isVisible(objectID)) {
+			Thread.sleep(1000);
+			timeout--;
+		}
+		if(timeout == 0){
+			throw new Exception("The Flex object:" + objectID + " was not visible. Time spent waiting:" + timeout); 
+		}
+	}
+
+	public void wait(int timeout) throws Exception
+	{
+		while(timeout > 0) {
+			Thread.sleep(1000);
+			timeout--;
+		}
+	}
+
+	public void type(String typeableObjId, String text) {
+		flashApp.call("doFlexType", typeableObjId, text);
+	}
+
+	public void click(String clickableObjId) {
+		flashApp.call("doFlexClick", clickableObjId, "");
+	}
+
+	public String text(String objId) {
+		return flashApp.call("getFlexText", objId, "");
+	}
+	
+		
+	
+	
+}
---
+++ java/flex-ui-selenium/test/com/thoughtworks/selenium/verifysetup/FlexUISeleniumTest.java
@@ -0,0 +1,45 @@
+package com.thoughtworks.selenium.verifysetup;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.FlexUISelenium;
+import com.thoughtworks.selenium.Selenium;
+
+/**
+ * This test only run properly if:
+ * selenium-server is running
+ * selenium-java-client-driver.jar and junit.jar have been added to the project or classpath
+ * flashselenium.jar has been added to the project or classpath
+ * flashselenium.jar has been added to the project or classpath
+ * flex-ui-selenium.jar has been added to the project or classpath
+ */
+public class FlexUISeleniumTest {
+	private final static String URL = "http://www.geocities.com/paulocaroli/flash/sum.html";
+	private Selenium selenium;
+	private FlexUISelenium flexUI;
+	
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("localhost", 4444, "*iexplore",URL);
+		selenium.start();
+		selenium.open(URL);
+		flexUI = new FlexUISelenium(selenium, "compareSumFlexObjId");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+
+	@Test
+	public void verifyFlexAppSumIsCorrect() {
+		flexUI.type("arg1", "2");
+		flexUI.type("arg2", "3");
+		flexUI.click("submit");
+		assertEquals("5", flexUI.text("result"));		
+	}
+	
+}
